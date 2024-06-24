44eba7cf437c106531ee352f9e518ec118a8587e
Daniel Steeples <daniel.steeples@outlook.com>
2024-06-24 16:03:21 +0100

    Spring init

---
+++ src/test/java/uk/co/danielsteeples/simplerestapi/SimpleRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.danielsteeples.simplerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
