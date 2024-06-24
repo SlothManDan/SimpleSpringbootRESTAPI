e1f6e296db58aab9c79765084dfd1fb8fae0a51d
Daniel Steeples <daniel.steeples@outlook.com>
2024-06-24 16:22:59 +0100

    user controller added

---
+++ src/main/java/uk/co/danielsteeples/simplerestapi/controllers/UserController.java
@@ -0,0 +1,48 @@
+package uk.co.danielsteeples.simplerestapi.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import uk.co.danielsteeples.simplerestapi.models.User;
+import uk.co.danielsteeples.simplerestapi.repos.UserRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable long id, @RequestBody User user) {
+        User existingUser = userRepository.findById(id).orElse(null);
+        if (existingUser != null) {
+            existingUser.setFirstName(user.getFirstName());
+            existingUser.setLastName(user.getLastName());
+            existingUser.setEmail(user.getEmail());
+            return userRepository.save(existingUser);
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable long id) {
+        userRepository.deleteById(id);
+    }
+}
